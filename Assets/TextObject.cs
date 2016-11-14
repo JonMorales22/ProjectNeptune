253b8bb5fa1c1d467a1e2f1698b4a3c05651a5f3
Jonathan Morales <jonmorales22@gmail.com>
2016-11-13 22:56:22 -0500

    Edited the MoveText script so that on start we move the words to a random spot off camera and then move them towards another spot where eventually the words will be readable.

---
+++ Assets/TextObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextObject : MonoBehaviour {
+	private Vector3 origin;
+	private Transform CoOrd1;
+	private Transform CoOrd2;
+	// Use this for initialization
+	void Start () {
+		origin = transform.position;
+	}
+
+	public Vector3 getOrigin()
+	{
+		return origin;
+	}
+
+}
--- Assets/_scripts/MoveTextScript.cs
+++ Assets/_scripts/MoveTextScript.cs
@@ -3,48 +3,95 @@ using System.Collections;
 
 public class MoveTextScript : MonoBehaviour {
 
-	private Vector3 newPos;
+	public float radius;
 
+	private Vector3 newPos;
 	private Transform[] wordArray;
+	private Vector3[] TargetArray;
 	private Rigidbody playerRb;
 	private float distance;
+
 	// Use this for initialization
 	void Start () {
 		distance = Mathf.Infinity;
 		playerRb = GameObject.FindWithTag("Player").GetComponent<Rigidbody> ();
 		wordArray = GetComponentsInChildren<Transform>();
+		TargetArray = new Vector3[wordArray.Length];
+		RandomText ();
 		StartCoroutine ("MoveText");
 	}
-	private void randomText()
+
+	/*Method does two things:
+	*	1:  Finds a place along a circle to place the words/
+	*		letters and then puts the words at that location.
+	*		(In this case I just coded for a circle of radius 10)
+	*
+	*	2:  Finds a point on the opposite side of the circle and saves that point in TargetArray
+	*/
+	private void RandomText()
 	{
-		
+		//Right now I'm just using -10 so that when we move the words they end up on the x axis and y axis
+		float x1 = -10;
+		float y1 = 0;
+		float x2, y2;
+		for (int i = 1; i < wordArray.Length; i++)
+		{
+			y1=GetPoint(x1);
+
+			//After we get points on circle we move the letters/words to that point
+			wordArray [i].position += new Vector3 (x1, y1, 0);
+
+			//we then find the corresponding point on the OPPOSITE end of the circle and store the point in TargetArray
+			x2 = x1 * -1;
+			y2 = GetPoint (x2);
+
+			// if the x1 is 0 then we can just multiply y1*-1
+			if (x2 != 0)
+				TargetArray [i] = new Vector3 (x2, y2, 0);
+			else
+				TargetArray [i] = new Vector3 (0, -y2, 0);
+
+			//right now I'm just adding by 10 to put the letters on the x axis and y axis
+			x1+=10;
+		}
 
 	}
 
+	/*Method just uses an equation for a circle to find where to put the words/letters
+	 *	equation: (x^2) + (y^2) = r^2
+	 */
+	private float GetPoint(float x)
+	{
+		float temp = radius * radius;
+		return Mathf.Sqrt (temp - (x * x));
+	}
+		
+	//Method that moves the letters/words from 1 point on the circle to the point on opposite side of the circle.
 	public IEnumerator MoveText()
 	{
-		Debug.Log ("Position: " + wordArray[1].transform.position);
+		//we use the distance formula to calculate how far the letters/words are from the target.
+		//If we fall within some range, we stop moving the letters/words
 		while(distance > 0.01f)
 		{
+			//we find the rate that we want to move the letters/words based on the player's velocity
 			float delta = playerRb.velocity.x/10;
-			//Debug.Log ("Delta: " + delta);
-			for (int i = 1; i < wordArray.Length; i++) {
-				newPos = new Vector3 (wordArray [i].position.x, 0, 0);
-				wordArray[i].position = Vector3.MoveTowards (wordArray[i].position, newPos, delta);
-				//wordArray [i].position  -= new Vector3(0,delta,0);
-			
+
+			for (int i = 1; i < wordArray.Length; i++)
+			{
+				newPos = new Vector3 (wordArray [i].position.x, wordArray[i].position.y, 0);
+				wordArray[i].position = Vector3.MoveTowards (wordArray[i].position, TargetArray[i], delta);
 			}
 
-			//wordArray[1].position
-
-			float xVal = Mathf.Abs(wordArray[1].position.x)*2;
-			float yVal = Mathf.Abs(wordArray[1].position.y)*2;
-
+			//use the distance formula: sqrt[((x1-x2)^2)-((y1-y2)^2)]
+			float xVal = Mathf.Abs(wordArray[1].position.x-TargetArray[1].x)*2;
+			float yVal = Mathf.Abs(wordArray[1].position.y-TargetArray[1].y)*2;
 			distance = Mathf.Sqrt (xVal + yVal);
-			Debug.Log ("Distance: " + distance);
+
 			yield return new WaitForSeconds (.01f);
 		}
 
 
+
 	}
+
 }
